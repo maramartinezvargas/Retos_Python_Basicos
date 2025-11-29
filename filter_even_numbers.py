570ab478eb64c2b50b8ced36521eb98064d6bcd9
tamamart <tamamart@student.42madrid.com>
2025-11-29 17:01:49 +0100

    Subject deleted

--- filter_even_numbers.py
+++ filter_even_numbers.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-2. Filtrar números pares
-Dada una lista de números enteros, genera una nueva lista que contenga únicamente los números pares utilizando comprensión de listas (list comprehension).
-"""
 
 nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
--- word_counter.py
+++ word_counter.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 def word_counter(text):
     text = text.lower()
