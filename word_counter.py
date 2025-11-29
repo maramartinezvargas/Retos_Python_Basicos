fbc3e42aa884727d232b34db2c6fbcc9b307c9ef
tamamart <tamamart@student.42madrid.com>
2025-11-29 16:36:50 +0100

    Ex01 word_counter.py

---
+++ word_counter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def word_counter(text):
+    text = text.lower()
+
+    counter = {}
+    
+    words = text.split()
+    
+    for word in words:
+        if word in counter:
+            counter[word] += 1
+        else:
+            counter[word] = 1
+    
+    return counter
+
+text = "Hola qué tal mundo Hola muy bien mundo"
+print(word_counter(text))
