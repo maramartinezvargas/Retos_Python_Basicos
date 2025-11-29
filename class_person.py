5d4a5dd8b75865c410fb19b797cd95c7a298be8d
tamamart <tamamart@student.42madrid.com>
2025-11-29 22:26:25 +0100

    Ex13 class_person.py

--- class_person.py
+++ class_person.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-### **13. Clase Persona**
-
-**Fichero:** `class_person.py`
-
-Define una clase `Person` con los atributos `name` y `age`. 
-Añade un método `introduce()` que muestre un mensaje utilizando ambos atributos.
-"""
-
 class Person:
     
     def __init__(self, name, age):
